84631c01de7b0da393c78be51987e486c673fc8b
sivagangaaa123 <sivagangaaa123@gmail.com>
2026-07-10 09:16:59 +0530

    Completed Library-Book-App

--- book-app/src/components/NavigationBar.jsx
+++ book-app/src/components/NavigationBar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Book App</a>
+    <Link class="navbar-brand" to="#">Book App</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="/">Add Book</a>
-        <a class="nav-link" href="search">Search Book</a>
-        <a class="nav-link" href="delete">Delete Book</a>
-        <a class="nav-link" href="view">View All Books</a>
+        <Link class="nav-link active" aria-current="page" to="/">Add Book</Link>
+        <Link class="nav-link" to="/search">Search Book</Link>
+        <Link class="nav-link" to="/delete">Delete Book</Link>
+        <Link class="nav-link" to="/view">View All Books</Link>
         
       </div>
     </div>
